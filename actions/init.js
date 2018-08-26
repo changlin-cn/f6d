cf95dbf464da454e229f3f17d82b5b65972cbeb9
hechanglin <lin-cn@users.noreply.github.com>
2018-08-26 16:00:51 +0800

    feat:Add  inquirer

--- actions/init.js
+++ actions/init.js
@@ -1,19 +1,25 @@
-const download = require('download-git-repo');
-const ora = require('ora');
+const download = require("download-git-repo");
+const ora = require("ora");
+const inquirer = require("inquirer");
 
-const reqositoryMap = require('../const/index.js').repository;
+const reqositoryMap = require("../const/index.js").repository;
 
-module.exports = function (repository, project='./', options) {
-    console.log(repository)
-    console.log(project)
-    const _repository = reqositoryMap[repository] ? reqositoryMap[repository] : repository
-    const spinner = ora(`"${_repository}" downloading...`);
+module.exports = async function(repository, project = "./", options) {
+  let rep = repository;
+  if (rep === undefined) {
+    rep = (await inquirer.prompt([
+      { type: "input", name: "rep", message: "Repository address:" }
+    ])).rep;
+  }
 
-    spinner.start();
-    download(_repository, project, function (err) {
-        if(err){
-            spinner.fail(`Download ${_repository} failed`)
-        }
-        spinner.succeed(`Download ${_repository} succeed`)
-    })
-}
+  const _repository = reqositoryMap[rep] ? reqositoryMap[rep] : rep;
+  const spinner = ora(`"${_repository}" downloading...`);
+
+  spinner.start();
+  download(_repository, project, function(err) {
+    if (err) {
+      spinner.fail(`Download ${_repository} failed`);
+    }
+    spinner.succeed(`Download ${_repository} succeed`);
+  });
+};
